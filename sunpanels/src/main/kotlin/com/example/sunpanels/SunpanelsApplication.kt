bef0b224c56a33bda40bf3e014e56f5e96b5f981
clindforsSejsing <c.lindforssejsing@gmail.com>
2023-01-02 16:31:46 +0100

    project setup kotlin, springboot, jpa, hibernate

---
+++ sunpanels/src/main/kotlin/com/example/sunpanels/SunpanelsApplication.kt
@@ -0,0 +1,12 @@
+package com.example.sunpanels
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SunpanelsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SunpanelsApplication>(*args)
+	println("Hello world")
+}
