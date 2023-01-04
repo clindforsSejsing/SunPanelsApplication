04c1c989a28f8e52e22afdf97e40f47adc8720f7
clindforsSejsing <c.lindforssejsing@gmail.com>
2023-01-04 16:11:04 +0100

    update javaversion to 19

---
+++ sunpanels/src/test/kotlin/com/example/sunpanels/SunpanelsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sunpanels
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SunpanelsApplicationTests {
+
+/*	@Test
+	fun contextLoads() {
+	}*/
+
+}
