c0374b7e44f942236778dcaf6c5011dc0c299758
clindforsSejsing <c.lindforssejsing@gmail.com>
2023-01-17 14:33:38 +0100

    Added docs for kotlin (Dokka), servicelayers

--- src/main/kotlin/com/example/sunpanels/service/SunPanelService.kt
+++ src/main/kotlin/com/example/sunpanels/service/SunPanelService.kt
@@ -9,6 +9,11 @@ import java.util.*
 @Service
 class SunPanelService(var sunPanelRepository: SunPanelRepository) {
 
+    /**
+     * @param sunPanel gets input from user, number of panels and electricprice in kr per kwh.
+     * @return 1 panel = 1.7 m x 1 m. Calculate and return area.
+     * */
+
     fun saveSunPanel(sunPanel: SunPanel): SunPanel? {
         val s = sunPanel
         val caclArea: Any = calculateArea(sunPanel.amountOfPanels)
@@ -24,6 +29,11 @@ class SunPanelService(var sunPanelRepository: SunPanelRepository) {
         return sunPanelRepository.findAll()
     }
 
+    /**
+     * @param sunPanel gets input from user, number of panels and electricprice in kr per kwh.
+     * @param sunpanelId gets the id of the sunpanel that we want to update
+     * @return saves the updated values of number of sunpanels, electricprice and area and returns it.
+     * */
     fun updateASunPanel(sunpanelId: Long, sunPanel: SunPanel): Any {
         val findingRightId: SunPanel = sunPanelRepository.findById(sunpanelId).get()
 
@@ -41,7 +51,6 @@ class SunPanelService(var sunPanelRepository: SunPanelRepository) {
 
     }
 
-    //add docs: 1 panel = 1.7 m x 1 m. Calculate and return area.
     fun calculateArea(nmbOfPanels: Int): Float {
         val areaOfAmountOfPanels: Float = (nmbOfPanels * (1.7 * 1)).toFloat()
         return areaOfAmountOfPanels
